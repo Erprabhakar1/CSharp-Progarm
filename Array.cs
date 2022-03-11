e61642f47bfff16e72114f0719a1b06a40bdc905
Erprabhakar1 <99798641+Erprabhakar1@users.noreply.github.com>
2022-03-11 12:44:52 +0530

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace my_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = new int[5];
+            myarray[0] = 7;
+            myarray[1] = 6;
+            myarray[2] = 4;
+            myarray[3] = 5;
+            myarray[4] = 8;
+            Console.WriteLine(myarray[0]);
+
+        }
+    }
+}
